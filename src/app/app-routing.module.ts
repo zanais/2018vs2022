3e26da11484b93e1bba6598164a66862c0edd7c5
zanais <sony_trevi@hotmail.com>
2022-04-06 05:01:16 -0500

    l

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './pages/main.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
